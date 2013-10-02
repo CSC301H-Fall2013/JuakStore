8ef84ba91fb58269142da65d20958e24a36f43c1
Tae Hahn <1hahntae@gmail.com>
2013-10-02 13:10:28 -0400

    Justification and manual test cases.

--- demo/juakstore/auth/views.py
+++ demo/juakstore/auth/views.py
@@ -1,26 +1,35 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
+
 def login_user(request):
     state = "Welcome."
-    username = password = ''
+    username = password = ""
+
+    # User types
+    group_one = "user"
+    group_two = "booker"
+    group_three = "admin"
 
     if request.POST:
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                login(request, user)
-                if user.groups.filter(name='admin').count():
-                    group_name = 'admin'
-                elif user.groups.filter(name='booker').count():
-                    group_name = 'booker'
-                elif user_groups.filter(name='user').count():
-                    group_name - 'user'
-                else: print("something is not right..")
-                state = "Login success. Group: " + group_name
+                if user.groups.filter(name=group_one).count():
+                    state = "Login success. Group: " + group_one
+                    login(request, user)
+                elif user.groups.filter(name=group_two).count():
+                    state = "Login success. Group: " + group_two
+                    login(request, user)
+                elif user.groups.filter(name=group_three).count():
+                    state = "Login success. Group: " + group_three
+                    login(request, user)
+                else:
+                    # Should put error-handling code here later..
+                    state = "weird"
             else:
                 state = "Deactived account."
         else:
